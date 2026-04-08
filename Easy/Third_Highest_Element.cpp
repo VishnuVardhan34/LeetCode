8628ad23dbe8e5e7496b30d9530462cbcccad69a
Vishnu Vardhan Nutalapati <166227351+VishnuVardhan34@users.noreply.github.com>
2026-04-09 02:44:59 +0530

    third maximum element

---
+++ Easy/Third_Highest_Element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long a = LONG_MIN, b = LONG_MIN, c = LONG_MIN;
+        for(int num : nums){
+            if(num == a || num == b || num == c){
+                continue;
+            }
+            if(num>a){
+                c=b;
+                b=a;
+                a=num;
+            }
+            else if(num>b){
+                c=b;
+                b=num;
+            }
+            else if(num>c){
+                c=num;
+            }
+        }
+        return (c==LONG_MIN)?a:c;
+    }
+};
