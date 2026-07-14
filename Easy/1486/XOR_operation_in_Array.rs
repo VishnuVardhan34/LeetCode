babd4e681773f1969a88bf204992d2878654cc42
Vishnu Vardhan Nutalapati <166227351+VishnuVardhan34@users.noreply.github.com>
2026-07-14 16:49:37 +0530

    Day-2

---
+++ Easy/1486/XOR_operation_in_Array.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn xor_operation(n: i32, start: i32) -> i32 {
+        let mut xor = 0;
+        for i in 0..n {
+            xor ^= start + 2 * i;
+        }
+        xor
+    }
+}
