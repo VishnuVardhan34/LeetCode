1e87e930e4752b3102e25a863622ae90d130ed89
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 12:24:06 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.rs
@@ -0,0 +1,20 @@
+// Last updated: 8/4/2026, 12:24:06 PM
+impl Solution {
+    pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        let mut ans = vec![0; 2];
+        let mut freq = vec![0; n + 1];
+        for num in nums {
+            freq[num as usize] += 1;
+        }
+        for i in 1..=n {
+            if freq[i] == 2 {
+                ans[0] = i as i32;
+            }
+            if freq[i] == 0 {
+                ans[1] = i as i32;
+            }
+        }
+        ans
+    }
+}
