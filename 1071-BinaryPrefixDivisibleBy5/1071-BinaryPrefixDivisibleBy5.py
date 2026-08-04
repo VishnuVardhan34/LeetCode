1667fe11e45edd75999e18b4bb5ce3d6a2b703b7
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 12:23:44 +0530

    Create 1071-BinaryPrefixDivisibleBy5

---
+++ 1071-BinaryPrefixDivisibleBy5/1071-BinaryPrefixDivisibleBy5.py
@@ -0,0 +1,11 @@
+# Last updated: 8/4/2026, 12:23:44 PM
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        ans = []
+        current = 0
+        
+        for bit in nums:
+            current = (current * 2 + bit) % 5
+            ans.append(current == 0)
+        
+        return ans
