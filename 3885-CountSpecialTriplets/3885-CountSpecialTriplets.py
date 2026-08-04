5ac278139be6719e2679a0d4f94fc31330fe32fc
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 12:21:58 +0530

    Create 3885-CountSpecialTriplets

---
+++ 3885-CountSpecialTriplets/3885-CountSpecialTriplets.py
@@ -0,0 +1,26 @@
+# Last updated: 8/4/2026, 12:21:59 PM
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        mod = 10**9 + 7
+        n = len(nums)
+        if n < 3:
+            return 0
+
+        maxv = max(nums)
+        size = maxv * 2 + 1
+
+        left = [0] * size
+        right = [0] * size
+
+        for v in nums:
+            right[v] += 1
+
+        ans = 0
+        for v in nums:
+            right[v] -= 1  # treat v as nums[j]
+            target = v * 2
+            if target < size:
+                ans = (ans + left[target] * right[target]) % mod
+            left[v] += 1
+
+        return ans % mod
