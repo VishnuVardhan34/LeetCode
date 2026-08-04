0791e848ef48b2de1e4f40c4aa364e5210817d6c
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 12:22:43 +0530

    Create 3194-FindWordsContainingCharacter

---
+++ 3194-FindWordsContainingCharacter/3194-FindWordsContainingCharacter.rs
@@ -0,0 +1,14 @@
+// Last updated: 8/4/2026, 12:22:43 PM
+impl Solution {
+    pub fn find_words_containing(words: Vec<String>, x: char) -> Vec<i32> {
+        let mut arr = Vec::new();
+
+        for i in 0..words.len() {
+            if words[i].contains(x) {
+                arr.push(i as i32);
+            }
+        }
+
+        arr
+    }
+}
