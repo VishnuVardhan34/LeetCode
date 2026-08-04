a241c0780b089a5cec6e8de0a48a3527d3d2abee
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 12:25:08 +0530

    Create 128-LongestConsecutiveSequence

---
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/4/2026, 12:25:08 PM
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        int longest=0;
+        for(int n: st){
+            if (st.count(n-1)) continue;
+            int curr = n;
+            int length=1;
+            while(st.count(curr+1)) {
+                curr++;
+                length++;
+            }
+            longest = max(longest, length);
+        }
+        return longest;
+    }
+};
