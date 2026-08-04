6869553ef43d03478b53192608a29b0aca4443f8
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 12:23:37 +0530

    Create 1411-ConvertBinaryNumberInALinkedListToInteger

---
+++ 1411-ConvertBinaryNumberInALinkedListToInteger/1411-ConvertBinaryNumberInALinkedListToInteger.py
@@ -0,0 +1,13 @@
+# Last updated: 8/4/2026, 12:23:38 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        result=0
+        while head:
+            result=(result*2)+head.val
+            head=head.next
+        return result
