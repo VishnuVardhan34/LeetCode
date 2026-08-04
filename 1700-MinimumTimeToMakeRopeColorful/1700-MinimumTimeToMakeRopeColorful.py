92678e0922c0e16d85e69b72723c844b9f60baf0
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 12:23:15 +0530

    Create 1700-MinimumTimeToMakeRopeColorful

---
+++ 1700-MinimumTimeToMakeRopeColorful/1700-MinimumTimeToMakeRopeColorful.py
@@ -0,0 +1,14 @@
+# Last updated: 8/4/2026, 12:23:15 PM
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        total_time=0
+        n=len(colors)
+
+        for i in range(1,n):
+            # if two consec have the same color
+            if colors[i] == colors[i-1]:
+                # Remove the one with smaller reach time
+                total_time += min(neededTime[i], neededTime[i-1])
+                neededTime[i] = max(neededTime[i], neededTime[i-1])
+
+        return total_time
