b16a048cc5935ed516615c3ec67bf4ca0e92a183
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 17:29:00 +0530

    File updated at 8/4/2026, 5:29:01 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -1,13 +1,22 @@
-// Last updated: 8/4/2026, 5:19:27 PM
+// Last updated: 8/4/2026, 5:29:01 PM
 1class Solution {
 2public:
-3    int maxArea(vector<int>& heights) {
-4        int area=0,l=0,r=heights.size()-1,maxArea=0;
-5        while(l<r) {
-6            area = min(heights[l], heights[r]) * (r-l);
-7            maxArea = max(maxArea, area);
-8            heights[l] < heights[r] ? l++ : r--;
-9        }
-10        return maxArea;
-11    }
-12};
+3    int trap(vector<int>& heights) {
+4        if (heights.empty()) return 0;
+5        int l=0,r=heights.size()-1;
+6        int lMax = heights[l], rMax = heights[r], ans=0;
+7        while(l<r) {
+8            if (lMax < rMax) {
+9                l++;
+10                lMax = max(lMax, heights[l]);
+11                ans += lMax  - heights[l];
+12            } else {
+13                r--;
+14                rMax = max(rMax, heights[r]);
+15                ans += rMax - heights[r];
+16            }
+17        }
+18        return ans;
+19    }
+20};
+21
