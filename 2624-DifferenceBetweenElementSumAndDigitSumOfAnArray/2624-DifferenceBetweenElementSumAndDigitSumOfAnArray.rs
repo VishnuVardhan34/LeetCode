f94509d2528d531d7c43893908cfa339c711666c
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 12:22:58 +0530

    Create 2624-DifferenceBetweenElementSumAndDigitSumOfAnArray

---
+++ 2624-DifferenceBetweenElementSumAndDigitSumOfAnArray/2624-DifferenceBetweenElementSumAndDigitSumOfAnArray.rs
@@ -0,0 +1,15 @@
+// Last updated: 8/4/2026, 12:22:58 PM
+impl Solution {
+    pub fn difference_of_sum(nums: Vec<i32>) -> i32 {
+        let element_sum: i32 = nums.iter().sum();
+        let mut digit_sum = 0;
+        for &num in &nums {
+            let mut n = num;
+            while n > 0 {
+                digit_sum += n % 10;
+                n /= 10;
+            }
+        }
+        return (element_sum - digit_sum).abs();
+    }
+}
