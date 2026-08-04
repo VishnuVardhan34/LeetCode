71f13ac0feddf77f068e4266617409d0d6727c83
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-04 12:22:24 +0530

    Create 3610-FindXSumOfAllKLongSubarraysI

---
+++ 3610-FindXSumOfAllKLongSubarraysI/3610-FindXSumOfAllKLongSubarraysI.py
@@ -0,0 +1,20 @@
+# Last updated: 8/4/2026, 12:22:24 PM
+from collections import Counter
+
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        n = len(nums)
+        ans = []
+        
+        for i in range(n - k + 1):
+            window = nums[i:i+k]
+            freq = Counter(window)
+
+            sorted_items = sorted(freq.items(), key=lambda p: (p[1], p[0]), reverse=True)
+
+            top_x = sorted_items[:x]
+
+            total = sum(num * freq[num] for num, _ in top_x)
+            ans.append(total)
+        
+        return ans
