3c72d03dc695df19ca62809ab7e72580935ae01b
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-06 12:42:16 +0530

    File updated at 8/6/2026, 12:42:17 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -1,25 +1,27 @@
-// Last updated: 8/6/2026, 12:41:26 PM
-1class Solution {
-2public:
-3    int characterReplacement(string s, int k) {
-4        unordered_map<char, int> freq;
-5
-6        int l = 0;
-7        int maxFreq = 0;
-8        int ans = 0;
-9
-10        for (int r = 0; r < s.size(); r++) {
-11            freq[s[r]]++;
-12            maxFreq = max(maxFreq, freq[s[r]]);
-13
-14            while ((r - l + 1) - maxFreq > k) {
-15                freq[s[l]]--;
-16                l++;
-17            }
-18
-19            ans = max(ans, r - l + 1);
-20        }
-21
-22        return ans;
-23    }
-24};
+// Last updated: 8/6/2026, 12:42:17 PM
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26, 0);
+
+        int left = 0;
+        int maxFreq = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            freq[s[right] - 'A']++;
+
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
