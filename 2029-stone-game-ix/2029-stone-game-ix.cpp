eccf902e366a7432daa3fc557aa77321a992e720
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-08-16 11:24:23 +0530

    Time: 0 ms (100%), Space: 131.2 MB (69.29%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0};
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+        int c0 = cnt[0], c1 = cnt[1], c2 = cnt[2];
+        if (c0 % 2 == 0) {
+            return (c1 > 0 && c2 > 0);
+        } else {
+            return abs(c1 - c2) > 2;
+        }
+    }
+};
