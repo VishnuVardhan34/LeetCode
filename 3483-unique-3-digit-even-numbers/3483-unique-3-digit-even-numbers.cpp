9294d543bc7817570c269898692b5901c2489d65
GameBoyr34 <166227351+VishnuVardhan34@users.noreply.github.com>
2026-09-11 13:09:50 +0530

    Time: 8 ms (56.12%), Space: 30.8 MB (76.02%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        vector<int> freq(10, 0);
-        for(int d: digits) {
-            freq[d]++;
-        }
-        int ans = 0;
-        for(int num = 100; num <= 998; num+= 2) {
-            int x = num;
-            int ones = x % 10;
-            x /= 10;
-            int tens = x % 10;
-            x /= 10;
-            int hunderds = x;
-            vector<int> used(10, 0);
-            used[hunderds]++;
-            used[tens]++;
-            used[ones]++;
-            bool possible = true;
-            for(int d = 0;d <= 9; d++) {
-                if(used[d] > freq[d]){
-                    possible = false;
-                    break;
+        unordered_set<int> s;
+        int n=digits.size();
+
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+                for(int k=0; k<n; k++){
+                    if(i!=j && i!=k && j!=k && digits[i] && !(digits[k]%2))
+                        s.insert(100*digits[i] + 10*digits[j] + digits[k]);
                 }
-            }
-            if(possible) {
-                ans++;
-            }
-        }
-        return ans;
+
+        return s.size();
     }
 };
